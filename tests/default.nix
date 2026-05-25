60c6f138e7f22e65d43c30763022cef5812aaac7
Allen Liao <wcliaw610@gmail.com>
2026-05-24 20:26:00 -0500

    feat: tests infra

--- tests/default.nix
+++ tests/default.nix
@@ -5,6 +5,67 @@
 }:
 
 let
-  args = { inherit nixpkgs lib ninx; };
+  test = name: t: {
+    inherit name;
+    run = { filename, index }:
+      if !t then 
+        builtins.warn "${filename}: Test ${toString index} \"${name}\" failed\n" t
+      else t;
+  };
+  testEq = name: expected: actual: {
+    inherit name;
+    run = { filename, index }: 
+      let 
+        result = expected == actual;
+      in
+      if !result then
+        builtins.warn ''
+          ${filename}: Test ${toString index} "${name}" failed
+            Expected: ${builtins.toJSON expected},
+            Actual: ${builtins.toJSON actual}
+          '' result
+      else result;
+  };
+  args = {
+    inherit
+      nixpkgs
+      lib
+      ninx
+      test
+      testEq
+      ;
+  };
+
+  testFile =
+    filename:
+      let
+        tests = import ./${filename} args;
+        stats = lib.foldl
+          (
+            { passed, failed, index }:
+            test: let
+              res = if test.run { inherit filename index; } then 1 else 0;
+            in {
+              passed = passed + res;
+              failed = failed + (1 - res);
+              index = index + 1;
+            }
+          )
+          {
+            passed = 0;
+            failed = 0;
+            index = 0;
+          }
+          tests;
+      in
+      builtins.trace "${filename}: ${toString stats.passed}/${toString (builtins.length tests)} tests passed\n" stats;
 in
-import ./serialize-eval.nix args
+
+lib.foldl 
+  (failed: stat:
+    failed + stat.failed) 
+  0
+  [
+  #(testFile "merge.nix")
+    (testFile "serialize-eval.nix")
+  ]
--- tests/serialize-eval.nix
+++ tests/serialize-eval.nix
@@ -1,6 +1,7 @@
 {
   nixpkgs,
   ninx,
+  testEq,
   ...
 }:
 
@@ -23,113 +24,137 @@ let
     (builtins.fromJSON (builtins.readFile script));
 in
 with ninx;
+[
+  (testEq "number" 123 (serEval 123))
 
-assert 123 == serEval 123;
+  (testEq "float" 123.123 (serEval 123.123))
 
-assert 123.123 == serEval 123.123;
+  (testEq "true" true (serEval true))
 
-assert true == (serEval true);
+  (testEq "false" false (serEval false))
 
-assert false == (serEval false);
+  (testEq "string" "a" (serEval "a"))
 
-assert "a" == serEval "a";
+  (testEq "attrs" { a = 1; } (serEval { a = 1; }))
 
-assert { a = 1; } == serEval { a = 1; };
+  (testEq
+    "rec-attrs"
+    rec {
+      a = b;
+      b = 1;
+    }
+    (serEval (rec-set {
+      a = var "b";
+      b = 1;
+    }))
+  )
 
-assert
-  rec {
-    a = b;
-    b = 1;
-  } == serEval (rec-set {
-    a = var "b";
-    b = 1;
-  });
+  (testEq
+    "path" 
+    (builtins.readFile ./default.nix) 
+    (builtins.readFile (serEval ./default.nix))
+  )
 
-assert (builtins.readFile ./default.nix) == (builtins.readFile (serEval ./default.nix));
+  (testEq
+    "operators"
+    10
+    (serEval (
+      op."+" [
+        (op."*" [
+          2
+          3
+        ])
+        4
+      ]
+    ))
+  )
 
-assert
-  10 == serEval (
-    op."+" [
-      (op."*" [
+
+  (testEq
+    "if-then-else"
+    (
+      if false then
+        1
+      else if true then
         2
+      else
         3
-      ])
-      4
-    ]
-  );
+    )
+    (serEval (
+      conds [
+        (ifthen false 1)
+        (ifthen true 2)
+      ] 3
+    ))
+  )
 
-assert
-  (
-    if false then
-      1
-    else if true then
-      2
-    else
-      3
-  ) == serEval (
-    conds [
-      (ifthen false 1)
-      (ifthen true 2)
-    ] 3
-  );
+  (testEq
+    "let-in"
+    {
+      a = 1;
+      b.c = 2;
+      b.d = 3;
+    }
+    (serEval (
+      letin {
+        r = {
+          a = 1;
+          b.c = 2;
+          b.d = 3;
+        };
+      } (var "r")
+    ))
+  )
 
-assert
-  {
-    a = 1;
-    b.c = 2;
-    b.d = 3;
-  } == serEval (
-    letin {
-      r = {
-        a = 1;
-        b.c = 2;
-        b.d = 3;
-      };
-    } (var "r")
-  );
 
-assert
-  7 == serEval (
-    app (raw "builtins.add") [
-      3
-      4
-    ]
-  );
+  (testEq
+    "application"
+    7
+    (serEval (
+      app (raw "builtins.add") [
+        3
+        4
+      ]
+    ))
+  )
 
-assert
-  8 == serEval (
-    letin {
-      fib = lambda "n" (
-        let
-          n = var "n";
-          eqn =
-            e:
-            op."==" [
-              n
-              e
-            ];
-          subn =
-            e:
-            op."-" [
-              n
-              e
-            ];
-        in
-        conds
-          [
-            (ifthen (op."||" [
-              (eqn 1)
-              (eqn 0)
-            ]) (var "n"))
-          ]
-          (
-            op."+" [
-              (app "fib" (subn 1))
-              (app "fib" (subn 2))
+
+  (testEq
+    "fibonacci"
+    8
+    (serEval (
+      letin {
+        fib = lambda "n" (
+          let
+            n = var "n";
+            eqn =
+              e:
+              op."==" [
+                n
+                e
+              ];
+            subn =
+              e:
+              op."-" [
+                n
+                e
+              ];
+          in
+          conds
+            [
+              (ifthen (op."||" [
+                (eqn 1)
+                (eqn 0)
+              ]) (var "n"))
             ]
-          )
-      );
-    } (app "fib" 6)
-  );
-
-true
+            (
+              op."+" [
+                (app "fib" (subn 1))
+                (app "fib" (subn 2))
+              ]
+            )
+        );
+      } (app "fib" 6)
+    ))
+  )
+]
